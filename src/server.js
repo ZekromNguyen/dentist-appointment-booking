b37d6e3ee90630b2134ac317a58230515cb43616
Nguyen Duc Tuyen <ndtuyen0604@gmail.com>
2024-05-20 16:19:31 +0700

    dentist-appointment

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebAllRoutes from "./routes/web";
+
+let app = express();
+
+//config view engine
+configViewEngine(app);
+
+//init all web routes
+initWebAllRoutes(app);
+
+let port = process.env.PORT;
+
+app.listen(port || 8080, () => {
+    console.log(`App is running at the port ${port || 8080}`);
+});
